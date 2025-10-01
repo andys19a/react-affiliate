93f0f331af19d1a71bcd2f95cf70b640bb933c29
andys19a <andre.alm@gritacademy.se>
2025-10-01 13:54:04 +0200

    Update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import Layout from "./components/Layout";
 function App() {
   return (
     <>
       <Header />
+      <Layout/>
       <Footer />
     </>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,26 @@
+import '../css/Layout.css';
+
 function Layout(){
 
     return (
 
         <div className="layout-page">
-
-
-
-
-
+            <div className="hero">
+                <p>En hero</p>
+            </div>
+            <div className='info'>
+                <p>En rubrik</p>
+                <p>Text</p>
+                <p>En knapp</p>
+            </div>
+            <div className='resultat'>
+                <p>Jämförelse</p>
+                <p>Resultat</p>
+            </div>
         </div>
 
 
     )
-
-
-
-
-
-
 }
+
+export default Layout;
