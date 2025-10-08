29fb3c8bd8bc76c90cc3974e876c148743b2dc19
andys19a <andre.alm@gritacademy.se>
2025-10-08 23:43:36 +0200

    Update tabel

--- src/components/Tabel.jsx
+++ src/components/Tabel.jsx
@@ -1,53 +1,49 @@
 import "../css/Table.css";
 
 const Table = () => {
-     const features = [
+  const features = [
     {
       name: "Antal spel per dag",
-      Trial: "50 om dagem",
+      Trial: "50 om dagen",
       Starter: "Obegränsat",
       Pro: "Obegränsat"
     },
     {
       name: "Value per bet",
-      Trial: "1-5%",
-      Starter: "1-7%",
+      Trial: "1–5%",
+      Starter: "1–7%",
       Pro: "Obegränsat"
     },
     {
       name: "Pris (beroende på valuta)",
       Trial: "Gratis",
-      Starter: "1300kr/mån",
-      Pro: "2300kr/mån"
+      Starter: "1300 kr/mån",
+      Pro: "2300 kr/mån"
     }
   ];
 
-
-
-return (
-  <table className="pricing-table">
-    <thead>
-      <tr>
-        <th>Funktioner</th>
-        <th>Vanlig version</th>
-        <th>Premium</th>
-        <th>Surebetting</th>
-      </tr>
-    </thead>
-    <tbody>
-      {features.map((feature, index) => (
-        <tr key={index}>
-          <td>{feature.name}</td>
-          <td>{feature.basic}</td>
-          <td>{feature.premium}</td>
-          <td>{feature.surebetting}</td>
+  return (
+    <table className="pricing-table">
+      <thead>
+        <tr>
+          <th>Funktioner</th>
+          <th>Trial</th>
+          <th>Starter</th>
+          <th>Pro</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
-
-
+      </thead>
+      <tbody>
+        {features.map((feature, index) => (
+          <tr key={index}>
+            <td>{feature.name}</td>
+            <td>{feature.Trial}</td>
+            <td>{feature.Starter}</td>
+            <td>{feature.Pro}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
